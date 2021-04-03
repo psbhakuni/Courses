100cee34d188c794663f0db356e4e42dbad30798
Prakash Bhakuni <psbhakuni@gmail.com>
2021-04-03 18:14:50 +0530

    fixed the issue in save functionlity on Add Course

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -1,29 +1,18 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import {Form, Button, Container, FormGroup, Input, Label} from 'reactstrap';
 import axios from "axios";
 import base_url from '../api/bootapi';
 
 const AddCourse = () => {
-    useEffect(()=>{
-        document.title='Add Course';
-    }, []);
-
     const [course, setCourse] = useState({});
-
-    //const handleChange = (event) => {
-     //   //alert("hi");
-     ///   setCourse(event.target.value);
-      //  console.log(course);
-      ///  alert(event.target.value);
-    //};
-
-    const handleSubmit = (event) => {
-        alert("hi");
+    const handleInputChange = e => {
+        console.log(course);
+        setCourse({ ...course, [e.target.name]: e.target.value });
+    };
+    const handleFinalSubmit = e => {
         console.log(course);
         postData(course);
-        event.preventDefault();
-        
-    };
+    };    
 
     const postData = (course) =>{
         axios.post(`${base_url}/courses`, course).then(
@@ -40,39 +29,37 @@ const AddCourse = () => {
     return (
         <Fragment>
             <h1 className="text-center my-3">Fill Course Details:</h1>
-            <Form onSubimt={handleSubmit}>
-                <FormGroup>
+            <Form >
+                <FormGroup key="id">
                     <Label for="id">Course Id</Label>
-                    <Input type="text" name="id" id="id" 
-                        placeholder="Course Id" 
-                        onChange={(e) => {
-                            console.log(e.target.value);
-                            setCourse({...course, id: e.target.value});
-                        }}                            
+                    <Input
+                        id="id"
+                        name="id"
+                        placeholder="Course Id"
+                        onChange={handleInputChange}
                     />
                 </FormGroup>
-                <FormGroup>
+                <FormGroup key="title">
                     <Label for="title">Course Title</Label>
-                    <Input type="text" name="title" id="title" 
-                        placeholder="Course Title" 
-                        onChange={(e) => {
-                            console.log(e.target.value);
-                            setCourse({...course, title: e.target.value});
-                        }}                            
-                    ></Input>
+                    <Input
+                        id="title"
+                        name="title"
+                        placeholder="Course Title"
+                        onChange={handleInputChange}
+                    />
                 </FormGroup>
-                <FormGroup>
-                    <Label for="description">Description</Label>
-                    <Input type="textarea" name="description" id="description" 
-                            placeholder="Course Description" style={{height:150}}                            
-                            onChange={(e) => {
-                                console.log(e.target.value);
-                                setCourse({...course, description: e.target.value});
-                            }}                            
-                    ></Input>
+                <FormGroup key="description">
+                    <Label for="description">Course Title</Label>
+                    <Input
+                        id="description"
+                        name="description"
+                        placeholder="Course description"
+                        onChange={handleInputChange}
+                    />                
                 </FormGroup>
+                
                 <Container className="text-center">
-                    <Button type="submit" color="primary">Add Course</Button>
+                    <Button onClick={handleFinalSubmit}>Submit</Button>
                     <Button color="secondary ml-2">Clear</Button>
                 </Container>
             </Form> 
